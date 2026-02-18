fa5117ee09750a943605c365d6a0c91b81233924
Diego Carmona Bernal <diego.carmona@xpertme.com>
2026-02-18 13:27:48 -0600

    Agregar modelos 'Categoria', 'Producto', 'Venta' y 'VentaDetalle' con relaciones y campos llenables

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+    protected $fillable = ['nombre', 'descripcion', 'precio', 'cantidad', 'imagen', 'categoria_id', 'activo'];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('categorias', CategoriaController::class);
